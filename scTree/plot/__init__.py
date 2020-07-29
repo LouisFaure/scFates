c157b6299aabc01f3495ee07187b5ecf80f092d5
Louis Le Boss <louis.faure75@gmail.com>
2020-07-29 21:19:56 +0200

    Added plotting functions

--- scTree/plot/__init__.py
+++ scTree/plot/__init__.py
@@ -2,3 +2,4 @@ from .tree import tree, tree_3d
 from .cluster import cluster
 from .test_association import test_association
 from .modules import modules
+from .milestones import milestones
--- scTree/tools/__init__.py
+++ scTree/tools/__init__.py
@@ -5,5 +5,5 @@ from .pseudotime import pseudotime
 from .test_association import test_association
 from .fit import fit
 from .cluster import cluster
-from .test_fork import test_fork, branch_specific, activation
+from .bifurcation_tools import test_fork, branch_specific, activation
 from .slide_cells import slide_cells
