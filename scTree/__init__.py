71bb9c6d50372b5e842f8209fc9fd961b4f01341
LouisFaure <louis.faure75@gmail.com>
2020-09-03 12:03:42 +0200

    cleanup

--- scTree/__init__.py
+++
@@ -1,4 +0,0 @@
-from anndata import AnnData
-
-from . import tl
-from . import pl
--- scTree/plot/__init__.py
+++
@@ -1,7 +0,0 @@
-from .tree import tree, tree_3d
-from .cluster import cluster
-from .test_association import test_association
-from .modules import modules
-from .milestones import milestones
-from .slide_cors import slide_cors
-from .synchro_path import synchro_path
