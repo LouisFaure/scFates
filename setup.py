1d8d38918599adb3f9eb4003c0eb4ebb9d9e0c5c
LouisFaure <louis.faure75@gmail.com>
2021-04-29 19:45:57 +0200

    fixed pypi readme [skip ci]

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import os
+from os import path
 from setuptools import setup, find_packages
-
+this_directory = path.abspath(path.dirname(__file__))
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
-try:
-    from pypandoc import convert
 
-    read_md = lambda f: convert(f, "rst")
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, "r").read()
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 on_rtd = os.environ.get("READTHEDOCS") == "True"
 if on_rtd:
@@ -20,7 +18,7 @@ setup(
     version_format="{tag}",
     setup_requires=["setuptools-git-version"],
     description="scanpy compatible python suite for fast tree inference and advanced pseudotime downstream analysis",
-    long_description=read_md("README.md"),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/LouisFaure/scFates",
     author="Louis Faure",
