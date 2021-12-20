e9c9c8ac1ecd79aff1e8d42789c30fe871694514
LouisFaure <louis.faure75@gmail.com>
2021-12-20 19:12:17 +0100

    Solving dendrogram generation issue by importing code from seaborn v0.11.1

--- scFates/tools/dendrogram.py
+++ scFates/tools/dendrogram.py
@@ -1,11 +1,23 @@
 import networkx as nx
 import random
-import seaborn as sns
 import warnings
 import numpy as np
 import pandas as pd
 import igraph
 import matplotlib.pyplot as plt
+import sys
+
+from .utils import ProgressParallel
+from joblib import delayed
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import warnings
+
+__all__ = ["swarmplot"]
+
 
 from .. import logging as logg
 from .. import settings
@@ -13,7 +25,7 @@ from .. import settings
 import anndata
 
 
-def dendrogram(adata: anndata.AnnData, crowdedness: float = 1):
+def dendrogram(adata: anndata.AnnData, crowdedness: float = 1, n_jobs: int = 1):
 
     """\
     Generate a  single-cell dendrogram embedding.
@@ -25,7 +37,7 @@ def dendrogram(adata: anndata.AnnData, crowdedness: float = 1):
     adata
         Annotated data matrix.
     crowdedness
-        size parameter passed on :func:`seaborn.swarmplot`, will influence wthe repartition of the cells along the segments.
+        will influence the repartition of the cells along the segments.
 
     Returns
     -------
@@ -73,36 +85,31 @@ def dendrogram(adata: anndata.AnnData, crowdedness: float = 1):
     for i, x in enumerate(newseg):
         df.loc[df.seg == newseg.index[i], "seg_pos"] = x
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=UserWarning)
-        ax = sns.swarmplot(x="seg_pos", y="t", data=df, size=crowdedness)
+    df.seg_pos = df.seg_pos.astype("category").cat.rename_categories(
+        range(len(df.seg_pos.unique()))
+    )
+    df.seg_pos = df.seg_pos.astype(int).astype("category")
 
-    plt.close()
-    segments = list()
-    ax.collections = ax.collections[-len(newseg) :]
-    for i in range(len(ax.collections)):
-        newseg = newseg.sort_values()
-        t_min, t_max = (
-            ax.collections[i].get_offsets()[:, 1].max(),
-            ax.collections[i].get_offsets()[:, 1].min(),
-        )
-        tip = graph["pp_seg"].loc[int(newseg.index[i])]["to"]
+    dend = swarmplot(
+        x="seg_pos", y="t", data=df, orient="v", size=crowdedness, n_jobs=n_jobs
+    )
+    adata.obsm["X_dendro"] = dend.loc[adata.obs_names].values
 
-        segments = segments + [[newseg.index[i], [(i, i), (t_min, t_max)]]]
+    # generate segments
+    newseg = newseg[newseg.argsort()]
 
-        for l in graph["pp_seg"].n[graph["pp_seg"]["from"] == tip]:
+    segments = []
+    for i, n in enumerate(newseg.index):
+        fro = graph["pp_seg"].loc[int(n), "from"]
+        to = graph["pp_seg"].loc[int(n), "to"]
+        t_min = -graph["pp_info"].loc[fro].time
+        t_max = -graph["pp_info"].loc[to].time
+
+        segments = segments + [[n, [(i, i), (t_min, t_max)]]]
+
+        for l in graph["pp_seg"].n[graph["pp_seg"]["from"] == to]:
             link = np.argwhere(newseg.index == str(l))[0][0]
             segments = segments + [[l, [(i, link), (t_max, t_max)]]]
-
-    axc_dct = zip(np.sort(df.seg_pos.unique()), ax.collections)
-    df = df.loc[df.t.sort_values().index]
-    new_emb = [
-        pd.DataFrame(axc.get_offsets(), index=df.loc[df.seg_pos == s].index)
-        for s, axc in axc_dct
-    ]
-    new_emb = pd.concat(new_emb).loc[adata.obs_names].values
-
-    adata.obsm["X_dendro"] = new_emb
 
     df = pd.DataFrame(segments)
     segments = df[0].unique()
@@ -114,7 +121,7 @@ def dendrogram(adata: anndata.AnnData, crowdedness: float = 1):
     logg.info("    finished", time=True, end=" " if settings.verbosity > 2 else "\n")
     logg.hint(
         "added \n"
-        "    .obs['X_dendro'], new embedding generated.\n"
+        "    .obsm['X_dendro'], new embedding generated.\n"
         "    .uns['dendro_segments'] tree segments used for plotting."
     )
 
@@ -193,3 +200,420 @@ def hierarchy_pos(G, root=None, width=1.0, vert_gap=0.2, vert_loc=0, xcenter=0.5
         return pos
 
     return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
+
+
+### All functions bellow have been adapted from seaborn v0.11.1
+### https://github.com/mwaskom/seaborn/blob/v0.11.1/seaborn/categorical.py
+
+
+class _CategoricalPlotter(object):
+
+    width = 0.8
+    default_palette = "light"
+    require_numeric = True
+
+    def establish_variables(
+        self,
+        x=None,
+        y=None,
+        hue=None,
+        data=None,
+        orient=None,
+        order=None,
+        hue_order=None,
+        units=None,
+    ):
+        """Convert input specification into a common representation."""
+
+        x = data.get(x, x)
+        y = data.get(y, y)
+        hue = data.get(hue, hue)
+        units = data.get(units, units)
+
+        # Validate the inputs
+        for var in [x, y, hue, units]:
+            if isinstance(var, str):
+                err = "Could not interpret input '{}'".format(var)
+                raise ValueError(err)
+
+        # Figure out the plotting orientation
+        vals, groups = y, x
+        # Put them into the common representation
+        plot_data = [np.asarray(vals)]
+
+        # Get a label for the value axis
+        if hasattr(vals, "name"):
+            value_label = vals.name
+        else:
+            value_label = None
+
+        # Get the categorical axis label
+        group_label = None
+        if hasattr(groups, "name"):
+            group_label = groups.name
+
+        # Get the order on the categorical axis
+        group_names = list(data.seg_pos.cat.categories)
+
+        plot_data, value_label = self._group_longform(vals, groups, group_names)
+
+        plot_hues = None
+        hue_title = None
+        plot_units = None
+
+        self.orient = orient
+        self.plot_data = plot_data
+        self.group_label = group_label
+        self.value_label = value_label
+        self.group_names = group_names
+        self.plot_hues = plot_hues
+        self.hue_title = hue_title
+
+    def _group_longform(self, vals, grouper, order):
+        """Group a long-form variable by another with correct order."""
+        # Ensure that the groupby will work
+        if not isinstance(vals, pd.Series):
+            if isinstance(grouper, pd.Series):
+                index = grouper.index
+            else:
+                index = None
+            vals = pd.Series(vals, index=index)
+
+        # Group the val data
+        grouped_vals = vals.groupby(grouper)
+        out_data = []
+        for g in order:
+            try:
+                g_vals = grouped_vals.get_group(g)
+            except KeyError:
+                g_vals = np.array([])
+            out_data.append(g_vals)
+
+        # Get the vals axis label
+        label = vals.name
+
+        return out_data, label
+
+
+class _CategoricalScatterPlotter(_CategoricalPlotter):
+
+    default_palette = "dark"
+    require_numeric = False
+
+    @property
+    def point_colors(self):
+        """Return an index into the palette for each scatter point."""
+        point_colors = []
+        for i, group_data in enumerate(self.plot_data):
+
+            # Initialize the array for this group level
+            group_colors = np.empty(group_data.size, int)
+            if isinstance(group_data, pd.Series):
+                group_colors = pd.Series(group_colors, group_data.index)
+
+            if self.plot_hues is None:
+
+                # Use the same color for all points at this level
+                # group_color = self.colors[i]
+                group_colors[:] = i
+
+            else:
+
+                # Color the points based on  the hue level
+
+                for j, level in enumerate(self.hue_names):
+                    # hue_color = self.colors[j]
+                    if group_data.size:
+                        group_colors[self.plot_hues[i] == level] = j
+
+            point_colors.append(group_colors)
+
+        return point_colors
+
+
+class _SwarmPlotter(_CategoricalScatterPlotter):
+    def __init__(
+        self, x, y, hue, data, order, hue_order, dodge, orient, color, palette, n_jobs
+    ):
+        """Initialize the plotter."""
+        self.establish_variables(x, y, hue, data, orient, order, hue_order)
+
+        # Set object attributes
+        self.dodge = dodge
+        self.width = 0.8
+        self.n_jobs = n_jobs
+
+    def could_overlap(self, xy_i, swarm, d):
+        """Return a list of all swarm points that could overlap with target.
+
+        Assumes that swarm is a sorted list of all points below xy_i.
+        """
+        _, y_i = xy_i
+        neighbors = []
+        for xy_j in reversed(swarm):
+            _, y_j = xy_j
+            if (y_i - y_j) < d:
+                neighbors.append(xy_j)
+            else:
+                break
+        return np.array(list(reversed(neighbors)))
+
+    def position_candidates(self, xy_i, neighbors, d):
+        """Return a list of (x, y) coordinates that might be valid."""
+        candidates = [xy_i]
+        x_i, y_i = xy_i
+        left_first = True
+        for x_j, y_j in neighbors:
+            dy = y_i - y_j
+            dx = np.sqrt(max(d ** 2 - dy ** 2, 0)) * 1.05
+            cl, cr = (x_j - dx, y_i), (x_j + dx, y_i)
+            if left_first:
+                new_candidates = [cl, cr]
+            else:
+                new_candidates = [cr, cl]
+            candidates.extend(new_candidates)
+            left_first = not left_first
+        return np.array(candidates)
+
+    def first_non_overlapping_candidate(self, candidates, neighbors, d):
+        """Remove candidates from the list if they overlap with the swarm."""
+
+        # IF we have no neighbours, all candidates are good.
+        if len(neighbors) == 0:
+            return candidates[0]
+
+        neighbors_x = neighbors[:, 0]
+        neighbors_y = neighbors[:, 1]
+
+        d_square = d ** 2
+
+        for xy_i in candidates:
+            x_i, y_i = xy_i
+
+            dx = neighbors_x - x_i
+            dy = neighbors_y - y_i
+
+            sq_distances = np.power(dx, 2.0) + np.power(dy, 2.0)
+
+            # good candidate does not overlap any of neighbors
+            # which means that squared distance between candidate
+            # and any of the neighbours has to be at least
+            # square of the diameter
+            good_candidate = np.all(sq_distances >= d_square)
+
+            if good_candidate:
+                return xy_i
+
+        # If `position_candidates` works well
+        # this should never happen
+        raise Exception(
+            "No non-overlapping candidates found. " "This should not happen."
+        )
+
+    def beeswarm(self, orig_xy, d):
+        """Adjust x position of points to avoid overlaps."""
+        # In this method, ``x`` is always the categorical axis
+        # Center of the swarm, in point coordinates
+        midline = orig_xy[0, 0]
+
+        # Start the swarm with the first point
+        swarm = [orig_xy[0]]
+
+        # Loop over the remaining points
+        for xy_i in orig_xy[1:]:
+
+            # Find the points in the swarm that could possibly
+            # overlap with the point we are currently placing
+            neighbors = self.could_overlap(xy_i, swarm, d)
+
+            # Find positions that would be valid individually
+            # with respect to each of the swarm neighbors
+            candidates = self.position_candidates(xy_i, neighbors, d)
+
+            # Sort candidates by their centrality
+            offsets = np.abs(candidates[:, 0] - midline)
+            candidates = candidates[np.argsort(offsets)]
+
+            # Find the first candidate that does not overlap any neighbours
+            new_xy_i = self.first_non_overlapping_candidate(candidates, neighbors, d)
+
+            # Place it into the swarm
+            swarm.append(new_xy_i)
+
+        return np.array(swarm)
+
+    def add_gutters(self, points, center, width):
+        """Stop points from extending beyond their territory."""
+        half_width = width / 2
+        low_gutter = center - half_width
+        off_low = points < low_gutter
+        if off_low.any():
+            points[off_low] = low_gutter
+        high_gutter = center + half_width
+        off_high = points > high_gutter
+        if off_high.any():
+            points[off_high] = high_gutter
+        return points
+
+    def swarm_points(self, ax, points, center, width, s, **kws):
+        """Find new positions on the categorical axis for each point."""
+        # Convert from point size (area) to diameter
+        default_lw = mpl.rcParams["patch.linewidth"]
+        lw = kws.get("linewidth", kws.get("lw", default_lw))
+        dpi = ax.figure.dpi
+        d = (np.sqrt(s) + lw) * (dpi / 72)
+        # Transform the data coordinates to point coordinates.
+        # We'll figure out the swarm positions in the latter
+        # and then convert back to data coordinates and replot
+        orig_xy = ax.transData.transform(points.get_offsets())
+
+        # Order the variables so that x is the categorical axis
+        if self.orient == "h":
+            orig_xy = orig_xy[:, [1, 0]]
+
+        # Do the beeswarm in point coordinates
+        new_xy = self.beeswarm(orig_xy, d)
+
+        # Transform the point coordinates back to data coordinates
+        if self.orient == "h":
+            new_xy = new_xy[:, [1, 0]]
+        new_x, new_y = ax.transData.inverted().transform(new_xy).T
+
+        # Add gutters
+        if self.orient == "v":
+            self.add_gutters(new_x, center, width)
+        else:
+            self.add_gutters(new_y, center, width)
+
+        # Reposition the points so they do not overlap
+        points.set_offsets(np.c_[new_x, new_y])
+
+    def swarm_points2(self, ax, points, center, width, s, **kws):
+        """Find new positions on the categorical axis for each point."""
+        # Convert from point size (area) to diameter
+        default_lw = mpl.rcParams["patch.linewidth"]
+        lw = kws.get("linewidth", kws.get("lw", default_lw))
+        dpi = ax.figure.dpi
+        d = (np.sqrt(s) + lw) * (dpi / 72)
+        # Transform the data coordinates to point coordinates.
+        # We'll figure out the swarm positions in the latter
+        # and then convert back to data coordinates and replot
+        orig_xy = ax.transData.transform(points.get_offsets())
+
+        # Order the variables so that x is the categorical axis
+        if self.orient == "h":
+            orig_xy = orig_xy[:, [1, 0]]
+
+        # Do the beeswarm in point coordinates
+        new_xy = self.beeswarm(orig_xy, d)
+
+        # Transform the point coordinates back to data coordinates
+        if self.orient == "h":
+            new_xy = new_xy[:, [1, 0]]
+        new_x, new_y = ax.transData.inverted().transform(new_xy).T
+
+        # Add gutters
+        if self.orient == "v":
+            self.add_gutters(new_x, center, width)
+        else:
+            self.add_gutters(new_y, center, width)
+
+        # Reposition the points so they do not overlap
+        return np.c_[new_x, new_y]
+
+    def draw_swarmplot(self, ax, kws):
+        """Plot the data."""
+        s = kws.pop("s")
+
+        centers = []
+        swarms = []
+
+        # Set the categorical axes limits here for the swarm math
+        if self.orient == "v":
+            ax.set_xlim(-0.5, len(self.plot_data) - 0.5)
+        else:
+            ax.set_ylim(-0.5, len(self.plot_data) - 0.5)
+
+        # Plot each swarm
+        cells = []
+        for i, group_data in enumerate(self.plot_data):
+            width = self.width
+            swarm_data = np.asarray(group_data)
+
+            # Sort the points for the beeswarm algorithm
+            sorter = np.argsort(swarm_data)
+            swarm_data = swarm_data[sorter]
+            cell = group_data.index[sorter]
+
+            # Plot the points in centered positions
+            cat_pos = np.ones(swarm_data.size) * i
+            points = ax.scatter(cat_pos, swarm_data, s=s, **kws)
+
+            cells.append(cell)
+            centers.append(i)
+            swarms.append(points)
+
+        # Autoscale the valus axis to set the data/axes transforms properly
+        ax.autoscale_view(scalex=self.orient == "h", scaley=self.orient == "v")
+
+        xys = ProgressParallel(
+            n_jobs=self.n_jobs,
+            total=len(self.plot_data),
+            file=sys.stdout,
+            desc="    segment ",
+        )(
+            delayed(self.swarm_points2)(ax, swarm, center, 0.8, s, **kws)
+            for center, swarm in zip(centers, swarms)
+        )
+        plt.close()
+        dend = pd.concat(
+            [pd.DataFrame(xy, index=cell) for cell, xy in zip(cells, xys)], axis=0
+        )
+        return dend
+
+    def plot(self, ax, kws):
+        """Make the full plot."""
+        return self.draw_swarmplot(ax, kws)
+
+
+def swarmplot(
+    *,
+    x=None,
+    y=None,
+    hue=None,
+    data=None,
+    order=None,
+    hue_order=None,
+    dodge=False,
+    orient=None,
+    color=None,
+    palette=None,
+    size=5,
+    edgecolor="gray",
+    linewidth=0,
+    ax=None,
+    n_jobs=1,
+    **kwargs
+):
+
+    if "split" in kwargs:
+        dodge = kwargs.pop("split")
+        msg = "The `split` parameter has been renamed to `dodge`."
+        warnings.warn(msg, UserWarning)
+
+    plotter = _SwarmPlotter(
+        x, y, hue, data, order, hue_order, dodge, orient, color, palette, n_jobs
+    )
+    if ax is None:
+        ax = plt.gca()
+
+    kwargs.setdefault("zorder", 3)
+    size = kwargs.get("s", size)
+    if linewidth is None:
+        linewidth = size / 10
+    if edgecolor == "gray":
+        edgecolor = "gray"
+    kwargs.update(dict(s=size ** 2, edgecolor=edgecolor, linewidth=linewidth))
+
+    dend = plotter.plot(ax, kwargs)
+    return dend
