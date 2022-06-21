94058ebc78f3d6947fd4bb104a8a3bbe7f05a3d1
LouisFaure <louis.faure75@gmail.com>
2022-06-21 11:56:58 +0200

    new milestone plot, PAGA style plot moved to pl.milestones_graph

--- scFates/plot/milestones.py
+++ scFates/plot/milestones.py
@@ -2,16 +2,108 @@ from typing_extensions import Literal
 import igraph
 import numpy as np
 import pandas as pd
-from typing import Union
+from typing import Union, Optional
 import matplotlib
 import matplotlib.cm as cm
 import matplotlib.colors as mcolors
 
 from ..tools.dendrogram import hierarchy_pos
 from .. import logging as logg
+from .utils import gen_milestones_gradients, get_basis, setup_axes
+from scanpy.plotting._utils import savefig_or_show
+import scanpy as sc
 
 
 def milestones(
+    adata,
+    basis: Union[None, str] = None,
+    annotate: bool = True,
+    title: str = "milestones",
+    ax=None,
+    show: Optional[bool] = None,
+    save: Union[str, bool, None] = None,
+    **kwargs,
+):
+    """\
+    Display the milestone graph in PAGA style.
+
+    Parameters
+    ----------
+    adata
+        Annotated data matrix.
+    basis
+        Reduction to use for plotting.
+    annotate
+        Display milestone labels on the plot.
+    ax
+        Add plot to existing ax.
+    show
+        show the plot.
+    save
+        save the plot.
+    kwargs
+        arguments to pass to :func:`matplotlib.pyplot.scatter`.
+
+    Returns
+    -------
+    If `show==False` an object of :class:`~matplotlib.axes.Axes`
+
+    """
+
+    basis = get_basis(adata, basis)
+    emb = adata.obsm[f"X_{basis}"]
+
+    if "sort_order" not in kwargs:
+        order = adata.obs.t.sort_values().index
+    else:
+        if kwargs["sort_order"]:
+            order = adata.obs.t.sort_values().index
+        else:
+            order = adata.obs_names
+
+    if ax is None:
+        ax = sc.pl.embedding(adata[order], basis=basis, alpha=0, show=False, **kwargs)
+    ax.scatter(
+        adata[order].obsm[f"X_{basis}"][:, 0],
+        adata[order].obsm[f"X_{basis}"][:, 1],
+        c=gen_milestones_gradients(adata)[order].values,
+        s=120000 / adata.shape[0] if "s" not in kwargs else kwargs["s"],
+        marker=".",
+        rasterized=True,
+    )
+
+    if annotate:
+        R = adata.obsm["X_R"]
+        proj = (np.dot(emb.T, R) / R.sum(axis=0)).T
+
+        X = proj[list(adata.uns["graph"]["milestones"].values()), :]
+        adata_m = sc.AnnData(
+            X,
+            dtype=X.dtype,
+            obs=dict(mil=list(adata.uns["graph"]["milestones"].keys())),
+            obsm={basis: X},
+        )
+        adata_m.obs["mil"] = adata_m.obs["mil"].astype("category")
+
+        sc.pl.embedding(
+            adata_m,
+            basis,
+            color="mil",
+            title=title,
+            legend_loc="on data",
+            ax=ax,
+            alpha=0,
+            legend_fontoutline=True,
+            show=False,
+        )
+
+    savefig_or_show("milestones", show=show, save=save)
+
+    if show == False:
+        return ax
+
+
+def milestones_graph(
     adata,
     color=None,
     cmap=None,
