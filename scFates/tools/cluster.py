400345a88b124223cfd7346f6bf8ddefb2910658
LouisFaure <louis.faure75@gmail.com>
2022-08-18 18:55:08 +0200

    reworked tl.cluster, now faster

--- scFates/tools/cluster.py
+++ scFates/tools/cluster.py
@@ -1,92 +1,121 @@
-from typing import Optional
-
-import numpy as np
-import pandas as pd
+import scanpy as sc
 from anndata import AnnData
-import scipy.sparse as sp
-
 from .. import logging as logg
 from .. import settings
-
-import phenograph
+from .graph_fitting import get_data
 
 
 def cluster(
     adata: AnnData,
-    knn: int = 10,
-    metric: str = "euclidean",
+    layer="fitted",
+    n_neighbors: int = 20,
+    n_pcs: int = 50,
+    metric: str = "cosine",
+    resolution: float = 1,
     device: str = "cpu",
     copy: bool = False,
-    n_jobs: int = 1,
 ):
-
     """\
-    Cluster feature trends.
-
-    The models are fit using *mgcv* R package. Note that since adata can currently only keep the
-    same dimensions for each of its layers, the dataset is subsetted to keep only significant
-    feratures.
+    Cluster features. Uses scanpy backend when using cpu, and cuml when using gpu.
+    Dataset is transposed, PCA is calulcated and a nearest neighbor graph is generated
+    from PC space. Leiden algorithm is used for community detection.
 
 
     Parameters
     ----------
     adata
         Annotated data matrix.
-    knn
+    layer
+        Layer of feature to calculate clusters, by default fitted features
+    n_neighbors
         Number of neighbors.
+    n_pcs
+        Number of PC to keep for PCA.
     metric
         distance metric to use for clustering.
+    resolution
+        Resolution parameter for leiden algorithm.
     device
         run the analysis on 'cpu' with phenograph, or on 'gpu' with grapheno.
-    leaves
-        restrain the fit to a subset of the tree (in combination with root).
+
     copy
         Return a copy instead of writing to adata.
     Returns
     -------
-
     adata : anndata.AnnData
         if `copy=True` it returns subsetted or else subset (keeping only
         significant features) and add fields to `adata`:
-
-        `.var['fit_clusters']`
-            cluster assignments for features.
-
+        `.var['cluters']`
+            module assignments for features.
     """
 
-    adata = data.copy() if copy else adata
+    layer = "X" if layer not in adata.layers else layer
+
+    logg.info(
+        f"Clustering features using {layer} layer",
+        reset=True,
+        end="\n",
+    )
+
+    X, use_rep = get_data(adata, layer, None)
+
+    adata_s = adata.copy().T
+    if device == "gpu":
+        from cuml.decomposition import PCA
+
+        pca = PCA(n_components=n_pcs)
+        adata_s.obsm["X_pca"] = pca.fit_transform(adata_s.X)
+    else:
+        sc.pp.pca(adata_s, n_comps=n_pcs)
+
+    sc.pp.neighbors(
+        adata_s,
+        n_neighbors=n_neighbors,
+        metric=metric,
+        method="rapids" if device == "gpu" else "umap",
+    )
 
     if device == "gpu":
-        from . import grapheno_modified
+        from scanpy.tools import _utils
 
-        logg.info("    clustering using grapheno")
-        communities, G, Q = grapheno_modified.cluster(
-            adata.layers["fitted"].T, metric=metric, n_neighbors=knn
+        adjacency = _utils._choose_graph(adata_s, None, None)
+        import cudf
+        import cugraph
+        from natsort import natsorted
+        import numpy as np
+        import pandas as pd
+
+        offsets = cudf.Series(adjacency.indptr)
+        indices = cudf.Series(adjacency.indices)
+
+        sources, targets = adjacency.nonzero()
+        weights = adjacency[sources, targets]
+        if isinstance(weights, np.matrix):
+            weights = weights.A1
+        weights = cudf.Series(weights)
+
+        g = cugraph.Graph()
+
+        if hasattr(g, "add_adj_list"):
+            g.add_adj_list(offsets, indices, weights)
+        else:
+            g.from_cudf_adjlist(offsets, indices, weights)
+
+        leiden_parts, _ = cugraph.leiden(g, resolution=resolution)
+        groups = (
+            leiden_parts.to_pandas()
+            .sort_values("vertex")[["partition"]]
+            .to_numpy()
+            .ravel()
         )
-        communities = communities.get()
-        G = sp.csr_matrix(G.to_numpy_array())
-
-    elif device == "cpu":
-        logg.info("    clustering using phenograph")
-        communities, G, Q = phenograph.cluster(
-            adata.layers["fitted"].T, primary_metric=metric, k=knn, n_jobs=n_jobs
+        adata_s.obs["leiden"] = pd.Categorical(
+            values=groups.astype("U"),
+            categories=natsorted(map(str, np.unique(groups))),
         )
-        G = sp.csr_matrix(G)
+    else:
+        sc.tl.leiden(adata_s, resolution=resolution)
 
-    adata.varp["similarity"] = G
-    adata.var["fit_clusters"] = communities
-
-    adata.uns["fit_cluster"] = dict(
-        parameters=dict(knn=knn, metric=metric, device=device, n_jobs=n_jobs),
-        modularity=Q,
-    )
+    adata.var["cluters"] = adata_s.obs.leiden
 
     logg.info("    finished", time=True, end=" " if settings.verbosity > 2 else "\n")
-    logg.hint(
-        "added\n"
-        "    .obsp['similarity'], pairwise similarity graph.\n"
-        "    .var['fit_clusters'], cluster assignments for features.\n"
-        "    .uns['fit_clusters'], parameters and modularity of the clustering."
-    )
-
-    return adata if copy else None
+    logg.hint("added \n" "    .var['cluters'] identified modules.")
