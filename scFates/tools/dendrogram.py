66c78ba829517d18f126a6ab566b3d80638b8e57
LouisFaure <louis.faure75@gmail.com>
2025-05-28 14:04:52 -0400

    faster dendrogram

--- scFates/tools/dendrogram.py
+++ scFates/tools/dendrogram.py
@@ -16,7 +16,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import warnings
 
-__all__ = ["swarmplot"]
+__all__ = ["stripplot"]
 
 
 from .. import logging as logg
@@ -25,7 +25,7 @@ from .. import settings
 import anndata
 
 
-def dendrogram(adata: anndata.AnnData, crowdedness: float = 1, n_jobs: int = 1):
+def dendrogram(adata: anndata.AnnData, crowdedness: float = 1):
 
     """\
     Generate a  single-cell dendrogram embedding.
@@ -90,8 +90,8 @@ def dendrogram(adata: anndata.AnnData, crowdedness: float = 1, n_jobs: int = 1):
     )
     df.seg_pos = df.seg_pos.astype(int).astype("category")
 
-    dend = swarmplot(
-        x="seg_pos", y="t", data=df, orient="v", size=crowdedness, n_jobs=n_jobs
+    dend = stripplot(
+        x="seg_pos", y="t", data=df, orient="v", size=crowdedness
     )
     adata.obsm["X_dendro"] = dend.loc[adata.obs_names].values
 
@@ -285,201 +285,48 @@ class _CategoricalScatterPlotter(_CategoricalPlotter):
     require_numeric = False
 
 
-class _SwarmPlotter(_CategoricalScatterPlotter):
+class _StripPlotter(_CategoricalScatterPlotter):
     def __init__(
-        self, x, y, hue, data, order, hue_order, dodge, orient, color, palette, n_jobs
+        self, x, y, hue, data, order, hue_order, orient, color, palette
     ):
         """Initialize the plotter."""
         self.establish_variables(x, y, hue, data, orient, order, hue_order)
 
         # Set object attributes
-        self.dodge = dodge
         self.width = 0.8
-        self.n_jobs = n_jobs
 
-    def could_overlap(self, xy_i, swarm, d):
-        """Return a list of all swarm points that could overlap with target.
-
-        Assumes that swarm is a sorted list of all points below xy_i.
-        """
-        _, y_i = xy_i
-        neighbors = []
-        for xy_j in reversed(swarm):
-            _, y_j = xy_j
-            if (y_i - y_j) < d:
-                neighbors.append(xy_j)
-            else:
-                break
-        return np.array(list(reversed(neighbors)))
-
-    def position_candidates(self, xy_i, neighbors, d):
-        """Return a list of (x, y) coordinates that might be valid."""
-        candidates = [xy_i]
-        x_i, y_i = xy_i
-        left_first = True
-        for x_j, y_j in neighbors:
-            dy = y_i - y_j
-            dx = np.sqrt(max(d ** 2 - dy ** 2, 0)) * 1.05
-            cl, cr = (x_j - dx, y_i), (x_j + dx, y_i)
-            if left_first:
-                new_candidates = [cl, cr]
-            else:
-                new_candidates = [cr, cl]
-            candidates.extend(new_candidates)
-            left_first = not left_first
-        return np.array(candidates)
-
-    def first_non_overlapping_candidate(self, candidates, neighbors, d):
-        """Remove candidates from the list if they overlap with the swarm."""
-
-        # IF we have no neighbours, all candidates are good.
-        if len(neighbors) == 0:
-            return candidates[0]
-
-        neighbors_x = neighbors[:, 0]
-        neighbors_y = neighbors[:, 1]
-
-        d_square = d ** 2
-
-        for xy_i in candidates:
-            x_i, y_i = xy_i
-
-            dx = neighbors_x - x_i
-            dy = neighbors_y - y_i
-
-            sq_distances = np.power(dx, 2.0) + np.power(dy, 2.0)
-
-            # good candidate does not overlap any of neighbors
-            # which means that squared distance between candidate
-            # and any of the neighbours has to be at least
-            # square of the diameter
-            good_candidate = np.all(sq_distances >= d_square)
-
-            if good_candidate:
-                return xy_i
-
-        # If `position_candidates` works well
-        # this should never happen
-        raise Exception(
-            "No non-overlapping candidates found. " "This should not happen."
-        )
-
-    def beeswarm(self, orig_xy, d):
-        """Adjust x position of points to avoid overlaps."""
-        # In this method, ``x`` is always the categorical axis
-        # Center of the swarm, in point coordinates
-        midline = orig_xy[0, 0]
-
-        # Start the swarm with the first point
-        swarm = [orig_xy[0]]
-
-        # Loop over the remaining points
-        for xy_i in orig_xy[1:]:
-
-            # Find the points in the swarm that could possibly
-            # overlap with the point we are currently placing
-            neighbors = self.could_overlap(xy_i, swarm, d)
-
-            # Find positions that would be valid individually
-            # with respect to each of the swarm neighbors
-            candidates = self.position_candidates(xy_i, neighbors, d)
-
-            # Sort candidates by their centrality
-            offsets = np.abs(candidates[:, 0] - midline)
-            candidates = candidates[np.argsort(offsets)]
-
-            # Find the first candidate that does not overlap any neighbours
-            new_xy_i = self.first_non_overlapping_candidate(candidates, neighbors, d)
-
-            # Place it into the swarm
-            swarm.append(new_xy_i)
-
-        return np.array(swarm)
-
-    def add_gutters(self, points, center, width):
-        """Stop points from extending beyond their territory."""
-        half_width = width / 2
-        low_gutter = center - half_width
-        off_low = points < low_gutter
-        if off_low.any():
-            points[off_low] = low_gutter
-        high_gutter = center + half_width
-        off_high = points > high_gutter
-        if off_high.any():
-            points[off_high] = high_gutter
-        return points
-
-    def swarm_points(self, ax, points, center, width, s, **kws):
-        """Find new positions on the categorical axis for each point."""
-        # Convert from point size (area) to diameter
-        default_lw = mpl.rcParams["patch.linewidth"]
-        lw = kws.get("linewidth", kws.get("lw", default_lw))
-        dpi = ax.figure.dpi
-        d = (np.sqrt(s) + lw) * (dpi / 72)
-        # Transform the data coordinates to point coordinates.
-        # We'll figure out the swarm positions in the latter
-        # and then convert back to data coordinates and replot
-        orig_xy = ax.transData.transform(points.get_offsets())
-
-        # Order the variables so that x is the categorical axis
-
-        # Do the beeswarm in point coordinates
-        new_xy = self.beeswarm(orig_xy, d)
-
-        # Transform the point coordinates back to data coordinates
-
-        new_x, new_y = ax.transData.inverted().transform(new_xy).T
-
-        # Add gutters
-
-        self.add_gutters(new_x, center, width)
-
-        # Reposition the points so they do not overlap
-        return np.c_[new_x, new_y]
-
-    def draw_swarmplot(self, ax, kws):
+    def draw_stripplot(self, ax, kws):
         """Plot the data."""
         s = kws.pop("s")
 
-        centers = []
-        swarms = []
-
         # Set the categorical axes limits here for the swarm math
-
         ax.set_xlim(-0.5, len(self.plot_data) - 0.5)
 
-        # Plot each swarm
+        # Plot each strip
+        xys = []
         cells = []
         for i, group_data in enumerate(self.plot_data):
-            width = self.width
-            swarm_data = np.asarray(group_data)
-
-            # Sort the points for the beeswarm algorithm
-            sorter = np.argsort(swarm_data)
-            swarm_data = swarm_data[sorter]
-            cell = group_data.index[sorter]
+            strip_data = np.asarray(group_data)
+            cell = group_data.index
 
             # Plot the points in centered positions
-            cat_pos = np.ones(swarm_data.size) * i
-            points = ax.scatter(cat_pos, swarm_data, s=s, **kws)
+            cat_pos = np.ones(strip_data.size) * i
+            
+            # For stripplot, we don't need complex positioning, just scatter
+            # We can add a small random jitter to x to avoid perfect overlap
+            jitter = (np.random.rand(strip_data.size) - 0.5) * self.width * 0.5
+            
+            x_coords = cat_pos + jitter
+            y_coords = strip_data
 
+            ax.scatter(x_coords, y_coords, s=s, **kws)
+            
+            xys.append(np.c_[x_coords, y_coords])
             cells.append(cell)
-            centers.append(i)
-            swarms.append(points)
 
-        # Autoscale the valus axis to set the data/axes transforms properly
+        # Autoscale the values axis to set the data/axes transforms properly
         ax.autoscale_view(scalex=self.orient == "h", scaley=self.orient == "v")
 
-        xys = ProgressParallel(
-            n_jobs=self.n_jobs,
-            total=len(self.plot_data),
-            file=sys.stdout,
-            desc="    segment ",
-        )(
-            delayed(self.swarm_points)(ax, swarm, center, 0.8, s, **kws)
-            for center, swarm in zip(centers, swarms)
-        )
-        plt.close()
         dend = pd.concat(
             [pd.DataFrame(xy, index=cell) for cell, xy in zip(cells, xys)], axis=0
         )
@@ -487,10 +334,10 @@ class _SwarmPlotter(_CategoricalScatterPlotter):
 
     def plot(self, ax, kws):
         """Make the full plot."""
-        return self.draw_swarmplot(ax, kws)
+        return self.draw_stripplot(ax, kws)
 
 
-def swarmplot(
+def stripplot(
     *,
     x=None,
     y=None,
@@ -498,7 +345,6 @@ def swarmplot(
     data=None,
     order=None,
     hue_order=None,
-    dodge=False,
     orient=None,
     color=None,
     palette=None,
@@ -506,12 +352,11 @@ def swarmplot(
     edgecolor="gray",
     linewidth=0,
     ax=None,
-    n_jobs=1,
     **kwargs
 ):
 
-    plotter = _SwarmPlotter(
-        x, y, hue, data, order, hue_order, dodge, orient, color, palette, n_jobs
+    plotter = _StripPlotter(
+        x, y, hue, data, order, hue_order, orient, color, palette
     )
     if ax is None:
         ax = plt.gca()
