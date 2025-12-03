ddd84d4cf8044d1915e01712160ca42a3f81dd9d
Sergey Isaev <serj.hoop@gmail.com>
2025-12-03 04:06:13 +0100

    Update graph_fitting.py
    EPG elastic curve was actually never added to adata.uns. It lead to errors during pseudotime calculation. Should work now.

--- scFates/tools/graph_fitting.py
+++ scFates/tools/graph_fitting.py
@@ -321,6 +321,7 @@ def tree(
             **kwargs,
         )
         adata.uns["graph"] = graph
+        adata.uns["epg"] = EPG
         adata.obsm["X_R"] = R
 
     if plot:
@@ -562,6 +563,7 @@ def curve_epg(
     EPG["Edges"] = list(EPG["Edges"])
 
     adata.uns["graph"] = graph
+    adata.uns["epg"] = EPG
     adata.obsm["X_R"] = R
 
     logg.info("    finished", time=True, end=" " if settings.verbosity > 2 else "\n")
@@ -623,6 +625,7 @@ def circle_epg(
     EPG["Edges"] = list(EPG["Edges"])[0]
 
     adata.uns["graph"] = graph
+    adata.uns["epg"] = EPG
     adata.obsm["X_R"] = R
 
     logg.info("    finished", time=True, end=" " if settings.verbosity > 2 else "\n")
